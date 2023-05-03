ee4e40542fc9eec0d1e573caff5a0b002b7e1a0f
Kissa41 <nastyagoncharenko5@gmail.com>
2023-05-03 14:09:36 +0200

    Added some components

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-35">
+        <div className="d-flex align-center mb-40 justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <div className="d-flex flex-wrap justify-center sneakers">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
